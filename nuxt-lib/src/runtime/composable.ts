e923f76196087db896ebc5898e3e1c339b36c6f3
mike <cspkno005@gmail.com>
2024-11-22 22:33:19 +0800

    update version

--- nuxt-lib/src/runtime/composable.ts
+++ nuxt-lib/src/runtime/composable.ts
@@ -1,24 +1,3 @@
 import { ESTest } from 'escss-estest'
 
 export { ESTest }
-
-type TYPES = 'undefined' | 'null' | 'array' | 'object' | 'boolean' | 'NaN' | 'number' | 'bigint' | 'string' | 'symbol' | 'function'
-/**
- * 100% function coverage for easier life. More: https://github.com/ESCSS-labs/ESCSS-ESTest
- * ```js
- * ESTest(NaN, 'NaN')
- * ESTest([], 'array')
- * ESTest(null, 'null')
- * ESTest(undefined, 'undefined')
- * ESTest(1, 'number')
- * ESTest('foo', 'string')
- * ESTest(true, 'boolean')
- * ESTest({}, 'object')
- * ESTest(1n, 'bigint')
- * ESTest(Symbol(), 'symbol')
- * ESTest(function () {}, 'function')
- * ESTest(1, 'object') // error
- * ESTest(1, 'object', 'foo') // error & message "foo"
- * ```
- */
-export declare function ESTest(input: unknown, type: TYPES, errMsg?: string): void
