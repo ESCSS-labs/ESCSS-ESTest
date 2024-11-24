85e6c4e48bd0c89cc3b9c506790fb531e163267c
mike <cspkno005@gmail.com>
2024-11-24 12:25:23 +0800

    refactor codebase

--- index.test.js
+++ index.test.js
@@ -1,66 +1,66 @@
 // Run test command (bun required): bun test
 import { describe, test, expect } from "bun:test";
-import { ESTest, _typeToken } from ".";
+import { ESTest, _testToken } from ".";
 
 describe("Normal Cases", () => {
   test("undefined", () => {
     ESTest(undefined, "undefined");
-    expect(_typeToken).toBe("undefined");
+    expect(_testToken).toBe("undefined");
   });
 
   test("null", () => {
     ESTest(null, "null");
-    expect(_typeToken).toBe("null");
+    expect(_testToken).toBe("null");
   });
 
   test("array", () => {
     ESTest([], "array");
-    expect(_typeToken).toBe("array");
+    expect(_testToken).toBe("array");
   });
 
   test("object", () => {
     ESTest({}, "object");
-    expect(_typeToken).toBe("object");
+    expect(_testToken).toBe("object");
   });
 
   test("boolean", () => {
     ESTest(true, "boolean");
-    expect(_typeToken).toBe("boolean");
+    expect(_testToken).toBe("boolean");
   });
 
   test("NaN", () => {
     ESTest(NaN, "NaN");
-    expect(_typeToken).toBe("NaN");
+    expect(_testToken).toBe("NaN");
   });
 
   test("number", () => {
     ESTest(123, "number");
-    expect(_typeToken).toBe("number");
+    expect(_testToken).toBe("number");
   });
 
   test("bigint", () => {
     ESTest(123n, "bigint");
-    expect(_typeToken).toBe("bigint");
+    expect(_testToken).toBe("bigint");
   });
 
   test("string", () => {
     ESTest("Hello World", "string");
-    expect(_typeToken).toBe("string");
+    expect(_testToken).toBe("string");
   });
 
   test("symbol", () => {
     ESTest(Symbol(), "symbol");
-    expect(_typeToken).toBe("symbol");
+    expect(_testToken).toBe("symbol");
   });
 
   test("function", () => {
     ESTest(function () {}, "function");
-    expect(_typeToken).toBe("function");
+    expect(_testToken).toBe("function");
   });
 
   test("custom error msg", () => {
     ESTest(123, "number", "foo");
-    expect(_typeToken).toBe("number");
+    expect(_testToken).toBe("number");
   });
 });
 
