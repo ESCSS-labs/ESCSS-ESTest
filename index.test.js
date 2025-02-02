d33c75d78c58e786b8d8d8b3572c2d12a2c68732
mike <cspkno005@gmail.com>
2025-02-03 00:52:16 +0800

    refactor: 💡 rewrite handleError system

--- index.test.js
+++ index.test.js
@@ -1,15 +1,9 @@
 // Run test command: bun test
 import { describe, test, expect } from 'bun:test';
-import { ESTest, _typeof, isDisabledLibrary } from '.';
+import { ESTest, _typeof } from '.';
 
 describe('Normal Cases', () => {  
   test('String', () => {
     expect(_typeof(ESTest('Hello World', 'string'))).toBe('object')
-  })
-})
-
-describe('Default Setting', () => {
-  test('isDisabledLibrary should be false', () => {
-    expect(isDisabledLibrary).toBe(false);
   })
 })
