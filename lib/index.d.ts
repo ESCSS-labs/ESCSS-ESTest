2b5171e9e42835e4df55bc3f4f8aeaeca290abb1
mike <cspkno005@gmail.com>
2025-02-07 00:27:44 +0800

    feat: 🎸 index.d.ts auto compeleted string & number

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
-type ALLOWED_TOKEN_TYPES = 'undefined' | 'null' | 'array' | 'date' | 'object' | 'boolean' | 'NaN' | 'number' | 'bigint' | 'string' | 'symbol' | 'function' | 'regex'
+declare type ALLOWED_TOKEN_TYPES = 'undefined' | 'null' | 'array' | 'date' | 'object' | 'boolean' | 'NaN' | 'number' | 'bigint' | 'string' | 'symbol' | 'function' | 'regex'
 /**
  * A JavaScript runtime testing library inspired by TDD, Joi, and Zod.
  * ```js
@@ -19,6 +19,132 @@ type ALLOWED_TOKEN_TYPES = 'undefined' | 'null' | 'array' | 'date' | 'object' | 
  * ESTest(1, 'object', 'foo') // public message "foo"
  * ```
  */
-export declare function ESTest(input: unknown, type: ALLOWED_TOKEN_TYPES, pubMsg?: string): void
+
+declare class chainString {
+  /**
+   * @example 
+   * // 'foo'.length <= 10
+   * ESTest('foo', 'string').max(10) // pass
+   */
+  static max():void
+
+  /**
+   * @example 
+   * // 'foo'.length >= 10
+   * ESTest('foo', 'string').min(10) // pass
+   */
+  static min():void
+
+  /**
+   * @example 
+   * // 'foo'.length === 10
+   * ESTest('foo', 'string').length(10) // pass
+   */
+  static length():void
+
+  /**
+   * @example 
+   * ESTest('foobar@gmail.com', 'string').email() // pass
+   */
+  static email():void
+
+  /**
+   * @example 
+   * ESTest(crypto.randomUUID(), 'string').uuid() // pass
+   */
+  static uuid():void
+
+  /**
+   * @example 
+   * ESTest('foo bar', 'string').regex(/(foo|bar)/) // pass
+   */
+  static regex():void
+
+  /**
+   * @example 
+   * ESTest('SGVsbG8gd29ybGQh', 'string').base64() // pass
+   */
+  static base64():void
+
+  /**
+   * @example 
+   * ESTest('192.168.1.1', 'string').ip() // pass ipv4
+   * ESTest('2001:0db8:85a3:0000:0000:8a2e:0370:7334', 'string').ip() // pass ipv6
+   */
+  static ip():void
+}
+
+declare class chainNumber {
+  /**
+   * @example 
+   * // 5 < 10
+   * ESTest(5, 'number').less(10) // pass
+   */
+  static less():void
+
+  /**
+   * @example 
+   * // 5 <= 10
+   * ESTest(5, 'number').max(10) // pass
+   */
+  static max():void
+
+  /**
+   * @example 
+   * // 15 > 10
+   * ESTest(15, 'number').greater(10) // pass
+   */
+  static greater():void
+
+  /**
+   * @example 
+   * // 15 >= 10
+   * ESTest(15, 'number').min(10) // pass
+   */
+  static min():void
+
+  /**
+   * @example 
+   * // Number.isInteger(15)
+   * ESTest(15, 'number').integer // pass 
+   */
+  static integer():void
+
+  /**
+   * @example 
+   * // 15 > 0
+   * ESTest(15, 'number').positive // pass 
+   */
+  static positive():void
+
+  /**
+   * @example 
+   * // -15 < 0
+   * ESTest(-15, 'number').negative // pass 
+   */
+  static negative():void
+
+  /**
+   * @example 
+   * // 15 % 3 === 0
+   * ESTest(15, 'number').multiple(3) // pass 
+   */
+  static multiple():void
+}
+
+declare class C {
+  static test3(): void;
+}
 
 
+type ReturnClassType<T extends ALLOWED_TOKEN_TYPES> = T extends 'string'
+  ? typeof chainString
+  : T extends 'number'
+  ? typeof chainNumber
+  : T extends 'object'
+  ? typeof C
+  : never;
+
+declare function test<T extends ALLOWED_TOKEN_TYPES>(input: string, type: T): ReturnClassType<T>;
+
+test(1, 'string')
