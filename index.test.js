1927a16f6a073706690f4474a54ded2f1d4fbb2d
mike <cspkno005@gmail.com>
2025-02-09 08:00:06 +0800

    feat: 🎸 base all types chain

--- index.test.js
+++ index.test.js
@@ -10,4 +10,48 @@ describe('Normal Cases', () => {
   test('Number', () => {
     expect(_typeof(ESTest(1, 'number'))).toBe('object')
   })
+
+  test('Array', () => {
+    expect(_typeof(ESTest([1, 2, 3], 'array'))).toBe('object')
+  })
+
+  test('Boolean', () => {
+    expect(_typeof(ESTest([], 'array'))).toBe('object')
+  })
+
+  test('Undefined', () => {
+    expect(_typeof(ESTest(undefined, 'undefined'))).toBe('object')
+  })
+
+  test('Null', () => {
+    expect(_typeof(ESTest(null, 'null'))).toBe('object')
+  })
+
+  test('Date', () => {
+    expect(_typeof(ESTest(new Date(), 'date'))).toBe('object')
+  })
+
+  test('object', () => {
+    expect(_typeof(ESTest({}, 'object'))).toBe('object')
+  })
+  
+  test('NaN', () => {
+    expect(_typeof(ESTest(NaN, 'NaN'))).toBe('object')
+  })
+
+  test('Bigint', () => {
+    expect(_typeof(ESTest(1n, 'bigint'))).toBe('object')
+  })
+
+  test('Symbol', () => {
+    expect(_typeof(ESTest(Symbol('test'), 'symbol'))).toBe('object')
+  })
+
+  test('Function', () => {
+    expect(_typeof(ESTest(function test() {}, 'function'))).toBe('object')
+  })
+
+  test('Regex', () => {
+    expect(_typeof(ESTest(/foo/, 'regex'))).toBe('object')
+  })
 })
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -26,52 +26,52 @@ declare class chainString {
    * // 'foo'.length <= 10
    * ESTest('foo', 'string').max(10) // pass
    */
-  static max():void
+  static max(): void
 
   /**
    * @example 
    * // 'foo'.length >= 10
    * ESTest('foo', 'string').min(10) // pass
    */
-  static min():void
+  static min(): void
 
   /**
    * @example 
    * // 'foo'.length === 10
    * ESTest('foo', 'string').length(10) // pass
    */
-  static length():void
+  static length(): void
 
   /**
    * @example 
    * ESTest('foobar@gmail.com', 'string').email() // pass
    */
-  static email():void
+  static email(): void
 
   /**
    * @example 
    * ESTest(crypto.randomUUID(), 'string').uuid() // pass
    */
-  static uuid():void
+  static uuid(): void
 
   /**
    * @example 
    * ESTest('foo bar', 'string').regex(/(foo|bar)/) // pass
    */
-  static regex():void
+  static regex(): void
 
   /**
    * @example 
    * ESTest('SGVsbG8gd29ybGQh', 'string').base64() // pass
    */
-  static base64():void
+  static base64(): void
 
   /**
    * @example 
    * ESTest('192.168.1.1', 'string').ip() // pass ipv4
    * ESTest('2001:0db8:85a3:0000:0000:8a2e:0370:7334', 'string').ip() // pass ipv6
    */
-  static ip():void
+  static ip(): void
 }
 
 declare class chainNumber {
@@ -80,71 +80,143 @@ declare class chainNumber {
    * // 5 < 10
    * ESTest(5, 'number').less(10) // pass
    */
-  static less():void
+  static less(): void
 
   /**
    * @example 
    * // 5 <= 10
    * ESTest(5, 'number').max(10) // pass
    */
-  static max():void
+  static max(): void
 
   /**
    * @example 
    * // 15 > 10
    * ESTest(15, 'number').greater(10) // pass
    */
-  static greater():void
+  static greater(): void
 
   /**
    * @example 
    * // 15 >= 10
    * ESTest(15, 'number').min(10) // pass
    */
-  static min():void
+  static min(): void
 
   /**
    * @example 
    * // Number.isInteger(15)
    * ESTest(15, 'number').integer // pass 
    */
-  static integer():void
+  static integer(): void
 
   /**
    * @example 
    * // 15 > 0
    * ESTest(15, 'number').positive // pass 
    */
-  static positive():void
+  static positive(): void
 
   /**
    * @example 
    * // -15 < 0
    * ESTest(-15, 'number').negative // pass 
    */
-  static negative():void
+  static negative(): void
 
   /**
    * @example 
    * // 15 % 3 === 0
    * ESTest(15, 'number').multiple(3) // pass 
    */
-  static multiple():void
+  static multiple(): void
+}
+
+declare class chainArray {
+  /**
+   * @example 
+   * // [1, 2, 3].length >= 3
+   * ESTest([1, 2, 3], 'array').min(3) // pass
+   */
+  static min(): void
+
+  /**
+   * @example 
+   * // [1, 2, 3].length <= 3
+   * ESTest([1, 2, 3], 'array').max(3) // pass
+   */
+  static max(): void
+
+  /**
+   * @example 
+   * // [1, 2, 3].length === 3
+   * ESTest([1, 2, 3], 'array').length(3) // pass
+   */
+  static length(): void
+
+}
+
+declare class chainBoolean {
+}
+
+declare class chainUndefined {
+}
+
+declare class chainNull {
+}
+
+declare class chainDate {
+}
+
+declare class chainObject {
+}
+
+declare class chainNaN {
+}
+
+declare class chainBigint {
+}
+
+declare class chainSymbol {
+}
+
+declare class chainFunction {
+}
+
+declare class chainRegex {
 }
 
 declare class C {
   static test3(): void;
 }
 
-
 type ReturnClassType<T extends ALLOWED_TOKEN_TYPES> = T extends 'string'
   ? typeof chainString
   : T extends 'number'
   ? typeof chainNumber
+  : T extends 'array'
+  ? typeof chainArray
+  : T extends 'boolean'
+  ? typeof chainBoolean
+  : T extends 'undefined'
+  ? typeof chainUndefined
+  : T extends 'null'
+  ? typeof chainNull
+  : T extends 'date'
+  ? typeof chainDate
   : T extends 'object'
-  ? typeof C
+  ? typeof chainObject
+  : T extends 'NaN'
+  ? typeof chainNaN
+  : T extends 'bigint'
+  ? typeof chainBigint
+  : T extends 'symbol'
+  ? typeof chainSymbol
+  : T extends 'function'
+  ? typeof chainFunction
+  : T extends 'regex'
+  ? typeof chainRegex
   : never;
 
-declare function test<T extends ALLOWED_TOKEN_TYPES>(input: string, type: T): ReturnClassType<T>;
+declare function ESTest<T extends ALLOWED_TOKEN_TYPES>(input: string, type: T, msg?: string): ReturnClassType<T>;
 
-test(1, 'string')
