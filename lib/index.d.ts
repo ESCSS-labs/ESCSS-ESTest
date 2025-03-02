ee5f56a2b81446da047a27c64e988d9028875316
mike <cspkno005@gmail.com>
2025-03-02 15:37:35 +0800

    feat: 🎸 add description() (_Common class)

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,186 +1,194 @@
 declare type _ALLOWED_TYPES = 'string' | 'number' | 'array' | 'object' | 'boolean' | 'date' | 'bigint' | 'undefined' | 'null' | 'NaN' | 'symbol' | 'function' | 'regex'
 
-
-declare type ChainType<T extends _ALLOWED_TYPES> =
-  T extends 'undefined' ? ChainUndefined
-  : T extends 'null' ? ChainNull
-  : T extends 'array' ? ChainArray
-  : T extends 'date' ? ChainDate
-  : T extends 'object' ? ChainObject
-  : T extends 'boolean' ? ChainBoolean
-  : T extends 'NaN' ? ChainNaN
-  : T extends 'number' ? ChainNumber
-  : T extends 'bigint' ? ChainBigint
-  : T extends 'string' ? ChainString
-  : T extends 'symbol' ? ChainSymbol
-  : T extends 'function' ? ChainFunction
-  : T extends 'regex' ? ChainRegex
+declare type _Chain<T extends _ALLOWED_TYPES> =
+  T extends 'string' ? _String
+  : T extends 'number' ? _Number
+  : T extends 'array' ? _Array
+  : T extends 'object' ? _Object
+  : T extends 'boolean' ? _Boolean
+  : T extends 'date' ? _Date
+  : T extends 'bigint' ? _Bigint
+  : T extends 'undefined' ? _Undefined
+  : T extends 'null' ? _Null
+  : T extends 'NaN' ? _NaN
+  : T extends 'symbol' ? _Symbol
+  : T extends 'function' ? _Function
+  : T extends 'regex' ? _Regex
   : never
 
-declare interface ChainUndefined {
+declare interface _Common<T extends _ALLOWED_TYPES> {
+  /**
+   * @example 
+   * // more information, doesn't do anything
+   * ESTest('foo', 'string').description('a helpful information')
+   */
+  description(): _Chain<T>
 }
 
-declare interface ChainNull {
-}
-
-declare interface ChainArray {
-  /**
-   * @example 
-   * // [1, 2, 3].length >= 3
-   * ESTest([1, 2, 3], 'array').min(3) // pass
-   */
-  min(): ChainType<'array'>
-
-  /**
-   * @example 
-   * // [1, 2, 3].length <= 3
-   * ESTest([1, 2, 3], 'array').max(3) // pass
-   */
-  max(): ChainType<'array'>
-
-  /**
-   * @example 
-   * // [1, 2, 3].length === 3
-   * ESTest([1, 2, 3], 'array').length(3) // pass
-   */
-  length(): ChainType<'array'>
-
-}
-
-declare interface ChainDate {
-}
-
-declare interface ChainObject {
-}
-
-declare interface ChainBoolean {
-}
-
-declare interface ChainNaN {
-}
-
-declare interface ChainNumber {
-  /**
-   * @example 
-   * // 5 < 10
-   * ESTest(5, 'number').less(10) // pass
-   */
-  less(): ChainType<'number'>
-
-  /**
-   * @example 
-   * // 5 <= 10
-   * ESTest(5, 'number').max(10) // pass
-   */
-  max(): ChainType<'number'>
-
-  /**
-   * @example 
-   * // 15 > 10
-   * ESTest(15, 'number').greater(10) // pass
-   */
-  greater(): ChainType<'number'>
-
-  /**
-   * @example 
-   * // 15 >= 10
-   * ESTest(15, 'number').min(10) // pass
-   */
-  min(): ChainType<'number'>
-
-  /**
-   * @example 
-   * // Number.isInteger(15)
-   * ESTest(15, 'number').integer // pass 
-   */
-  integer(): ChainType<'number'>
-
-  /**
-   * @example 
-   * // 15 > 0
-   * ESTest(15, 'number').positive // pass 
-   */
-  positive(): ChainType<'number'>
-
-  /**
-   * @example 
-   * // -15 < 0
-   * ESTest(-15, 'number').negative // pass 
-   */
-  negative(): ChainType<'number'>
-
-  /**
-   * @example 
-   * // 15 % 3 === 0
-   * ESTest(15, 'number').multiple(3) // pass 
-   */
-  multiple(): ChainType<'number'>
-}
-
-declare interface ChainBigint {
-}
-
-declare interface ChainString {
+declare interface _String extends _Common<'string'> {
   /**
    * @example 
    * // 'foo'.length <= 10
    * ESTest('foo', 'string').max(10) // pass
    */
-  max(): ChainType<'string'>
+  max(): _Chain<'string'>
 
   /**
    * @example 
    * // 'foo'.length >= 10
    * ESTest('foo', 'string').min(10) // pass
    */
-  min(): ChainType<'string'>
+  min(): _Chain<'string'>
 
   /**
    * @example 
    * // 'foo'.length === 10
    * ESTest('foo', 'string').length(10) // pass
    */
-  length(): ChainType<'string'>
+  length(): _Chain<'string'>
 
   /**
    * @example 
    * ESTest('foobar@gmail.com', 'string').email() // pass
    */
-  email(): ChainType<'string'>
+  email(): _Chain<'string'>
 
   /**
    * @example 
    * ESTest(crypto.randomUUID(), 'string').uuid() // pass
    */
-  uuid(): ChainType<'string'>
+  uuid(): _Chain<'string'>
 
   /**
    * @example 
    * ESTest('foo bar', 'string').regex(/(foo|bar)/) // pass
    */
-  regex(): ChainType<'string'>
+  regex(): _Chain<'string'>
 
   /**
    * @example 
    * ESTest('SGVsbG8gd29ybGQh', 'string').base64() // pass
    */
-  base64(): ChainType<'string'>
+  base64(): _Chain<'string'>
 
   /**
    * @example 
    * ESTest('192.168.1.1', 'string').ip() // pass ipv4
    * ESTest('2001:0db8:85a3:0000:0000:8a2e:0370:7334', 'string').ip() // pass ipv6
    */
-  ip(): ChainType<'string'>
+  ip(): _Chain<'string'>
 }
 
-declare interface ChainSymbol {
+declare interface _Number extends _Common<'number'> {
+  /**
+   * @example 
+   * // 5 < 10
+   * ESTest(5, 'number').less(10) // pass
+   */
+  less(): _Chain<'number'>
+
+  /**
+   * @example 
+   * // 5 <= 10
+   * ESTest(5, 'number').max(10) // pass
+   */
+  max(): _Chain<'number'>
+
+  /**
+   * @example 
+   * // 15 > 10
+   * ESTest(15, 'number').greater(10) // pass
+   */
+  greater(): _Chain<'number'>
+
+  /**
+   * @example 
+   * // 15 >= 10
+   * ESTest(15, 'number').min(10) // pass
+   */
+  min(): _Chain<'number'>
+
+  /**
+   * @example 
+   * // Number.isInteger(15)
+   * ESTest(15, 'number').integer // pass 
+   */
+  integer(): _Chain<'number'>
+
+  /**
+   * @example 
+   * // 15 > 0
+   * ESTest(15, 'number').positive // pass 
+   */
+  positive(): _Chain<'number'>
+
+  /**
+   * @example 
+   * // -15 < 0
+   * ESTest(-15, 'number').negative // pass 
+   */
+  negative(): _Chain<'number'>
+
+  /**
+   * @example 
+   * // 15 % 3 === 0
+   * ESTest(15, 'number').multiple(3) // pass 
+   */
+  multiple(): _Chain<'number'>
 }
 
-declare interface ChainFunction {
+declare interface _Array extends _Common<'array'> {
+  /**
+   * @example 
+   * // [1, 2, 3].length >= 3
+   * ESTest([1, 2, 3], 'array').min(3) // pass
+   */
+  min(): _Chain<'array'>
+
+  /**
+   * @example 
+   * // [1, 2, 3].length <= 3
+   * ESTest([1, 2, 3], 'array').max(3) // pass
+   */
+  max(): _Chain<'array'>
+
+  /**
+   * @example 
+   * // [1, 2, 3].length === 3
+   * ESTest([1, 2, 3], 'array').length(3) // pass
+   */
+  length(): _Chain<'array'>
+
 }
 
-declare interface ChainRegex {
+declare interface _Object {
+}
+
+declare interface _Boolean {
+}
+
+declare interface _Date {
+}
+
+declare interface _Bigint {
+}
+
+declare interface _Undefined {
+}
+
+declare interface _Null {
+}
+
+declare interface _NaN {
+}
+
+declare interface _Symbol {
+}
+
+declare interface _Function {
+}
+
+declare interface _Regex {
 }
 
 /**
@@ -221,7 +229,7 @@ declare interface ChainRegex {
  * }
  * 
  */
-export declare function ESTest<T extends _ALLOWED_TYPES>(input: unknown, type: T, pubMsg?: string): ChainType<T>
+export declare function ESTest<T extends _ALLOWED_TYPES>(input: unknown, type: T, pubMsg?: string): _Chain<T>
 
 /**
  * A JavaScript runtime testing library inspired by TDD, Joi, and Zod.
@@ -267,4 +275,4 @@ export declare function ESTest<T extends _ALLOWED_TYPES>(input: unknown, type: T
  * })
  * 
  */
-export declare function unSafeESTest<T extends _ALLOWED_TYPES>(input: unknown, type: T, pubMsg?: string): ChainType<T>
+export declare function unSafeESTest<T extends _ALLOWED_TYPES>(input: unknown, type: T, pubMsg?: string): _Chain<T>
