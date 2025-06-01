efea26891dc6fb9fe7d08400f2bc7847e5446f7a
mike <cspkno005@gmail.com>
2025-06-01 19:43:29 +0800

    refactor: 💡 update globalThis.__ESCSS_ESTEST__.information

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 globalThis.__ESCSS_ESTEST__ = {
-  information: `
-   ========================================================================
-   | name: escss-estest                                                   |
-   | version: 2.1.5                                                       |
-   | license: AGPL-3.0-only OR Commercial                                 ｜
-   | author: Mike Lee                                                     |
-   ========================================================================
-  `,
+  information: "name: escss-estest, version: 2.1.5, license: AGPL-3.0-only OR Commercial, author: Mike Lee",
   message: "Set 'globalThis.__ESCSS_ESTEST__.message' for customize message",
   isESTestDisabled: false,
   analysis: {
