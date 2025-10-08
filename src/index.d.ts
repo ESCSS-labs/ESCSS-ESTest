a2b0b93292d2abffb3feaa7328f0d622ba8f1f5d
mike <cspkno005@gmail.com>
2025-10-08 11:23:28 +0800

    fix: 🐛 auto-complete does not work properly

--- src/index.d.ts
+++ src/index.d.ts
@@ -10,40 +10,48 @@ declare type _ALLOWED_TYPES =
   | "object"
   | "array";
 
-declare type _SCHEMA =
-  | _ALLOWED_TYPES
-  | { [key: string]: _SCHEMA | _SCHEMA[] }
-  | _SCHEMA[];
+type _ArraySchema<Type> = Type extends (infer U)[] ? U : Type;
 
-declare type _ClassType<T extends _ALLOWED_TYPES> = T extends "undefined"
+type _ObjectSchema<Type> = {
+  [P in keyof Type]: Type[P] extends (infer U)[]
+    ? [U extends object ? _ObjectSchema<U> : _ALLOWED_TYPES]
+    : Type[P] extends object
+      ? _ObjectSchema<Type[P]>
+      : _ALLOWED_TYPES;
+};
+
+declare type _ClassType<
+  Type extends _ALLOWED_TYPES,
+  Input,
+> = Type extends "undefined"
   ? _Undefined
-  : T extends "null"
+  : Type extends "null"
     ? _Null
-    : T extends "boolean"
+    : Type extends "boolean"
       ? _Boolean
-      : T extends "number"
+      : Type extends "number"
         ? _Number
-        : T extends "bigint"
+        : Type extends "bigint"
           ? _BigInt
-          : T extends "string"
+          : Type extends "string"
             ? _String
-            : T extends "symbol"
+            : Type extends "symbol"
               ? _Symbol
-              : T extends "function"
+              : Type extends "function"
                 ? _Function
-                : T extends "object"
-                  ? _Object
-                  : T extends "array"
-                    ? _Array
+                : Type extends "object"
+                  ? _Object<Input extends object ? Input : object> // <-- 傳遞 INPUT，並確保它是物件
+                  : Type extends "array"
+                    ? _Array<Input>
                     : never;
 
-declare interface _Common<T extends _ALLOWED_TYPES> {
+declare interface _Common<Type extends _ALLOWED_TYPES, Input = unknown> {
   /**
    * @example
    * // more information, doesn't do anything
    * ESTest('foo', 'string').describe('a helpful information')
    */
-  describe(): _ClassType<T>;
+  describe(): _ClassType<Type, Input>;
 }
 
 declare interface _Undefined extends _Common<"undefined"> {}
@@ -58,56 +66,56 @@ declare interface _Number extends _Common<"number"> {
    * // 5 < 10
    * ESTest(5, 'number').less(10) // pass
    */
-  less(): _ClassType<"number">;
+  less(): _Number;
 
   /**
    * @example
    * // 5 <= 10
    * ESTest(5, 'number').max(10) // pass
    */
-  max(): _ClassType<"number">;
+  max(): _Number;
 
   /**
    * @example
    * // 15 > 10
    * ESTest(15, 'number').greater(10) // pass
    */
-  greater(): _ClassType<"number">;
+  greater(): _Number;
 
   /**
    * @example
    * // 15 >= 10
    * ESTest(15, 'number').min(10) // pass
    */
-  min(): _ClassType<"number">;
+  min(): _Number;
 
   /**
    * @example
    * // Number.isInteger(15)
    * ESTest(15, 'number').integer() // pass
    */
-  integer(): _ClassType<"number">;
+  integer(): _Number;
 
   /**
    * @example
    * // 15 > 0
    * ESTest(15, 'number').positive() // pass
    */
-  positive(): _ClassType<"number">;
+  positive(): _Number;
 
   /**
    * @example
    * // -15 < 0
    * ESTest(-15, 'number').negative() // pass
    */
-  negative(): _ClassType<"number">;
+  negative(): _Number;
 
   /**
    * @example
    * // 15 % 3 === 0
    * ESTest(15, 'number').multiple(3) // pass
    */
-  multiple(): _ClassType<"number">;
+  multiple(): _Number;
 }
 
 declare interface _BigInt extends _Common<"bigint"> {
@@ -116,49 +124,49 @@ declare interface _BigInt extends _Common<"bigint"> {
    * // 5n < 10n
    * ESTest(5n, 'bigint').less(10n) // pass
    */
-  less(): _ClassType<"number">;
+  less(): _BigInt;
 
   /**
    * @example
    * // 5n <= 10n
    * ESTest(5n, 'bigint').max(10n) // pass
    */
-  max(): _ClassType<"number">;
+  max(): _BigInt;
 
   /**
    * @example
    * // 15n > 10n
    * ESTest(15n, 'bigint').greater(10n) // pass
    */
-  greater(): _ClassType<"number">;
+  greater(): _BigInt;
 
   /**
    * @example
    * // 15n >= 10n
    * ESTest(15n, 'bigint').min(10n) // pass
    */
-  min(): _ClassType<"number">;
+  min(): _BigInt;
 
   /**
    * @example
    * // 15n > 0n
    * ESTest(15n, 'bigint').positive() // pass
    */
-  positive(): _ClassType<"number">;
+  positive(): _BigInt;
 
   /**
    * @example
    * // -15n < 0n
    * ESTest(-15n, 'bigint').negative() // pass
    */
-  negative(): _ClassType<"number">;
+  negative(): _BigInt;
 
   /**
    * @example
    * // 15n % 3n === 0n
    * ESTest(15n, 'bigint').multiple(3n) // pass
    */
-  multiple(): _ClassType<"number">;
+  multiple(): _BigInt;
 }
 
 declare interface _String extends _Common<"string"> {
@@ -167,21 +175,21 @@ declare interface _String extends _Common<"string"> {
    * // 'foo'.length <= 10
    * ESTest('foo', 'string').max(10) // pass
    */
-  max(): _ClassType<"string">;
+  max(): _String;
 
   /**
    * @example
    * // 'foo'.length >= 10
    * ESTest('foo', 'string').min(10) // pass
    */
-  min(): _ClassType<"string">;
+  min(): _String;
 
   /**
    * @example
    * // 'foo'.length === 10
    * ESTest('foo', 'string').length(10) // pass
    */
-  length(): _ClassType<"string">;
+  length(): _String;
 
   /**
    * @example
@@ -197,67 +205,67 @@ declare interface _String extends _Common<"string"> {
    * // A loose regex that allows Unicode characters, enforces length limits, and that's about it.
    * ESTest("user.name123@example-domain.com", "string").email("unicodeEmail"); // pass
    */
-  email(): _ClassType<"string">;
+  email(): _String;
 
   /**
    * @example
    * ESTest('550e8400-e29b-41d4-a716-446655440000', 'string').uuid4() // pass
    */
-  uuid4(): _ClassType<"string">;
+  uuid4(): _String;
 
   /**
    * @example
    * ESTest('0189c7e4-3b8a-7e3b-8291-4e6f2b1a4c7d', 'string').uuid7() // pass
    */
-  uuid7(): _ClassType<"string">;
+  uuid7(): _String;
 
   /**
    * @example
    * ESTest('foo bar', 'string').regex(/(foo|bar)/) // pass
    */
-  regex(): _ClassType<"string">;
+  regex(): _String;
 
   /**
    * @example
    * ESTest('SGVsbG8gd29ybGQh', 'string').base64() // pass
    */
-  base64(): _ClassType<"string">;
+  base64(): _String;
 
   /**
    * @example
    * ESTest('Zm9vYmFyXzEyMw', 'string').base64url() // pass
    */
-  base64url(): _ClassType<"string">;
+  base64url(): _String;
 
   /**
    * @example
    * ESTest('192.168.1.1', 'string').ip4() // pass
    */
-  ip4(): _ClassType<"string">;
+  ip4(): _String;
 
   /**
    * @example
    * ESTest('2001:0db8:85a3:0000:0000:8a2e:0370:7334', 'string').ip6() // pass
    */
-  ip6(): _ClassType<"string">;
+  ip6(): _String;
 
   /**
    * @example
    * ESTest('192.168.0.0/16', 'string').cidr4() // pass
    */
-  cidr4(): _ClassType<"string">;
+  cidr4(): _String;
 
   /**
    * @example
    * ESTest('2001:0db8:85a3:0000:0000:8a2e:0370:7334/16', 'string').cidr6() // pass
    */
-  cidr6(): _ClassType<"string">;
+  cidr6(): _String;
 
   /**
    * @example
    * ESTest('🌀', 'string').emoji() // pass
    */
-  emoji(): _ClassType<"string">;
+  emoji(): _String;
 
   /**
    * @example
@@ -266,50 +274,49 @@ declare interface _String extends _Common<"string"> {
    * ESTest('+14151234567', 'string').e164() // pass
    *
    */
-  e164(): _ClassType<"string">;
+  e164(): _String;
 
   /**
    * @example
    * ESTest('foobar', 'string').lowercase() // pass
    */
-  lowercase(): _ClassType<"string">;
+  lowercase(): _String;
 }
 
 declare interface _Symbol extends _Common<"symbol"> {}
 
 declare interface _Function extends _Common<"function"> {}
 
-declare interface _Object extends _Common<"object"> {
+declare interface _Object<Input extends object = object>
+  extends _Common<"object", Input> {
   /**
    * @example
-   * Note: schema() only accepts 'object'.
+   * // Note: schema() only accepts 'object'.
    *
-   * ESTest(data, 'object', 'custom error msg').schema({
-   *   name: 'string',
-   *   'msg?': 'string',
-   *   more: {
-   *     a: 'number',
-   *     'b?': 'number',
+   * ESTest(data, 'object', 'demo is {}').schema({
+   *   a: 'string',
+   *   demo: {
+   *     x: 'number',
+   *     'y?': 'number',
    *   }
    * })
    *
-   * ESTest(data, 'object', 'custom error msg').schema({
-   *   name: 'string',
-   *   'msg?': 'string',
-   *   more: [{
-   *     a: 'number',
-   *     'b?': 'number',
+   * ESTest(data, 'object', 'demo is {}[]').schema({
+   *   a: 'string',
+   *   demo: [{
+   *     x: 'number',
+   *     'y?': 'number',
    *   }]
    * })
    */
-  schema(T: { [key: string]: _SCHEMA }): _ClassType<"object">;
+  schema(arg: _ObjectSchema<Input>): _Object<Input>;
 
   /**
    * @example
    * // "single case" cross field validation
    *
    * const data = {
-   *   password: '123'
+   *   password: '123',
    *   checkPassword: '123'
    * }
    *
@@ -321,16 +328,16 @@ declare interface _Object extends _Common<"object"> {
    *  })
    *  .refine(val => val.password === val.checkPassword, 'password mismatch')
    */
-  refine<T>(fn: (arg: T) => boolean, message: string): void;
+  refine(fn: (arg: Input) => boolean, message: string): void;
 
   /**
    * @example
    * // "multiple cases" cross field validation
    *
-   * const data {
+   * const data = {
    *   name: 'mike',
    *   checkName: 'mike',
-   *   password: '123'
+   *   password: '123',
    *   checkPassword: '123'
    * }
    *
@@ -345,50 +352,53 @@ declare interface _Object extends _Common<"object"> {
    *  .superRefine((val, ctx) => {
    *    // case 1
    *    if (val.name !== val.checkName) {
-   *      ctx.addIssue('name mismatch')
+   *      ctx.addIssue('name mismatch') // create an error msg
    *    }
    *
    *    // case 2
    *    if (val.password !== val.checkPassword) {
-   *      ctx.addIssue('password mismatch')
+   *      ctx.addIssue('password mismatch') // create an error msg
    *    }
    *  })
    */
-  superRefine<T>(
+  superRefine(
     fn: (
-      arg: T,
+      arg: Input,
       ctx: {
+        /**
+         * create an error msg
+         */
         addIssue: (message: string) => void;
       },
     ) => void,
   ): void;
 }
 
-declare interface _Array extends _Common<"array"> {
+declare interface _Array<Input> extends _Common<"array", Input> {
   /**
    * @example
    * // [1, 2, 3].length >= 3
    * ESTest([1, 2, 3], 'array').min(3) // pass
    */
-  min(): _ClassType<"array">;
+  min(): _Array<Input>;
 
   /**
    * @example
    * // [1, 2, 3].length <= 3
    * ESTest([1, 2, 3], 'array').max(3) // pass
    */
-  max(): _ClassType<"array">;
+  max(): _Array<Input>;
 
   /**
    * @example
    * // [1, 2, 3].length === 3
    * ESTest([1, 2, 3], 'array').length(3) // pass
    */
-  length(): _ClassType<"array">;
+  length(): _Array<Input>;
 
   /**
    * @example
-   * Note: schema() only accepts 'object'.
+   * // Note: schema() only accepts 'object'.
    *
    * ESTest(data, 'array', 'custom error msg').schema({
    *   name: 'string',
@@ -408,30 +418,91 @@ declare interface _Array extends _Common<"array"> {
    *   }]
    * })
    */
-  schema(T: { [key: string]: _SCHEMA }): _ClassType<"array">;
-}
+  schema(
+    arg: _ArraySchema<Input> extends object
+      ? _ObjectSchema<_ArraySchema<Input>>
+      : never,
+  ): _Array<Input>;
 
-declare interface _Array extends _Common<"array"> {}
+  /**
+   * @example
+   * // "single case" cross field validation
+   *
+   * const data = [{
+   *   password: '123',
+   *   checkPassword: '123'
+   * }]
+   *
+   * // pass
+   * ESTest(data, 'array')
+   *  .schema({
+   *    password: 'string',
+   *    checkPassword: 'string',
+   *  })
+   *  .refine(val => val[0].password === val[0].checkPassword, 'password mismatch')
+   */
+  refine(fn: (arg: Input) => boolean, message: string): void;
+
+  /**
+   * @example
+   * // "multiple cases" cross field validation
+   *
+   * const data = [{
+   *   name: 'mike',
+   *   checkName: 'mike',
+   *   password: '123',
+   *   checkPassword: '123'
+   * }]
+   *
+   * // pass
+   * ESTest(data, 'array')
+   *  .schema({
+   *    name: 'string',
+   *    checkName: 'string',
+   *    password: 'string',
+   *    checkPassword: 'string',
+   *  })
+   *  .superRefine((val, ctx) => {
+   *    // case 1
+   *    if (val[0].name !== val[0].checkName) {
+   *      ctx.addIssue('name mismatch')
+   *    }
+   *
+   *    // case 2
+   *    if (val[0].password !== val[0].checkPassword) {
+   *      ctx.addIssue('password mismatch')
+   *    }
+   *  })
+   */
+  superRefine(
+    fn: (
+      arg: Input,
+      ctx: {
+        addIssue: (message: string) => void;
+      },
+    ) => void,
+  ): void;
+}
 
 /**
  * Non-breaking error logging via console.error(...)
  * @see https://github.com/ESCSS-labs/ESCSS-ESTest
  */
-export declare function ESTest<K, T extends _ALLOWED_TYPES>(
-  input: K,
-  type: T,
+export declare function ESTest<Type extends _ALLOWED_TYPES, Input>(
+  input: Input,
+  type: Type,
   message?: string,
-): _ClassType<T>;
+): _ClassType<Type, Input>;
 
 /**
  * Breaking error throwing via throw new Error(...)
  * @see https://github.com/ESCSS-labs/ESCSS-ESTest
  */
-export declare function unSafeESTest<T extends _ALLOWED_TYPES>(
-  input: unknown,
-  type: T,
+export declare function unSafeESTest<Type extends _ALLOWED_TYPES, Input>(
+  input: Input,
+  type: Type,
   message?: string,
-): _ClassType<T>;
+): _ClassType<Type, Input>;
 
 /**
  * @example
@@ -443,11 +514,11 @@ export declare function unSafeESTest<T extends _ALLOWED_TYPES>(
  *
  * @see https://github.com/ESCSS-labs/ESCSS-ESTest
  */
-export declare function ESTestForLibrary<T extends _ALLOWED_TYPES>(
-  input: unknown,
-  type: T,
+export declare function ESTestForLibrary<Type extends _ALLOWED_TYPES, Input>(
+  input: Input,
+  type: Type,
   message?: string,
-): _ClassType<T>;
+): _ClassType<Type, Input>;
 
 declare global {
   var __ESCSS_ESTEST__: {
@@ -463,7 +534,7 @@ declare global {
      *  - true: Enables a high-performance mode for production use.
      *  - false (default): Logs detailed errors for debugging.
      *
-     * *Note: unSafeESTest() will not be affected (for security reasons)*
+     * Note: unSafeESTest() will not be affected (for security reasons)*
      *
      * @see https://github.com/ESCSS-labs/ESCSS-ESTest
      */
